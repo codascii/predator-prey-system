392ace6a6bd4dd6ceb23913f293d06d8e5ebd969
Fisiikakala Maunakea <fiakaifonufleur@gmail.com>
2019-03-15 16:45:44 +0100

    suite creation class lapin

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-class Fox {
-    constructor() {
-        this.
+class Lapin {
+    constructor(actif, multiplicite, vitesse, freqApparition, nbLapin, x,y, id) {
+        this.actif = actif;
+        this.multiplicite = multiplicite;
+        this.vitesse = vitesse;
+        this.freqApparition = freqApparition;
+        this.nbLapin = nbLapin;
+        this.multiplicite = multiplicite
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    seMultiplier(){
+
+    }
+
+    seDeplacer(){
+
+    }
+
+    meurt(){
+
     }
 }
