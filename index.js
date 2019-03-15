080f306e876b0ac5675599ba1c6689e64464cc87
zeidane97 <agbzeidane18@gmail.com>
2019-03-15 16:46:57 +0100

    ajput des classes

---
+++ index.js
@@ -0,0 +1,25 @@
+class  Renard {
+
+    constructor(vitesse, frequence_apparition, distance,dureeF, nbr_renard) {
+        this.vitesse = vitesse;
+        this.frequence_apparition = frequence_apparition;
+        this.distance= distnace;
+        this.dureeF = dureeF;
+        this.nbr_renard = 10;
+    }
+
+   SeDeplacent(){
+        return ;
+    }
+
+
+    MangeLapin(){
+
+        return;
+    }
+
+    Meurt(){
+         
+        return;
+    }
+}
