4b6c8b9a8d5c3ba2d5ad71c37105e599a12b7cdf
zeidane97 <agbzeidane18@gmail.com>
2019-03-15 18:04:21 +0100

    Modification de l'interface

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+class  Renard {
+
+    constructor(vitesse, frequence_apparition, distance,dureeF, nbr_renard) {
+        this.vitesse = vitesse;
+        this.frequence_apparition = frequence_apparition;
+        this.distance= distnace;
+        this.dureeF = dureeF;
+        this.nbr_renard = 10;
+    }
+
+   SeDeplacent(){
+        return ;
+    }
+
+
+    MangeLapin(){
+
+        return;
+    }
+
+    Meurt(){
+         
+        return;
+    }
+}
+
 class Lapin {
     constructor(actif, multiplicite, vitesse, freqApparition, nbLapin, x,y, id) {
         this.actif = actif;
@@ -37,29 +64,3 @@ var c = canvas.getContext('2d');
 c.fillRect(200, 100, 100, 100);
 c.fillRect(300, 100, 100, 100);
 console.log(canvas);*/
-
-class  Renard {
-
-    constructor(vitesse, frequence_apparition, distance,dureeF, nbr_renard) {
-        this.vitesse = vitesse;
-        this.frequence_apparition = frequence_apparition;
-        this.distance= distnace;
-        this.dureeF = dureeF;
-        this.nbr_renard = 10;
-    }
-
-   SeDeplacent(){
-        return ;
-    }
-
-
-    MangeLapin(){
-
-        return;
-    }
-
-    Meurt(){
-         
-        return;
-    }
-}
