211d0a8ac67cd2c1a15017c84548329d7d3ad39c
Codascii <dev.codascii@gmail.com>
2019-03-15 16:46:47 +0100

    Modif js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-class Fox {
-    constructor() {
-        this.
-    }
-}
+
+
+
+
+
+var canvas = document.querySelector('canvas#app');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Context
+var c = canvas.getContext('2d');
+/*c.fillRect(100, 100, 100, 100);
+c.fillRect(200, 100, 100, 100);
+c.fillRect(300, 100, 100, 100);
+console.log(canvas);*/
