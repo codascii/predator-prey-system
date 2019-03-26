6ac772352936e24029e89f7377d5091568b99f08
Codascii <dev.codascii@gmail.com>
2019-03-26 17:18:36 +0100

    Affichage des lapin en prenant en compte le nombre initial

--- index.js
+++ index.js
@@ -15,12 +15,7 @@ class  Renard {
     }
 
     creer() {
-        /*c.beginPath();
-        c.arc(this.x, this.y, this.rayon, 0, Math.PI * 2, false);
-        c.strokeStyle = 'blue';
-        c.stroke();*/
-
-        var img = document.getElementById("fox");
+        const img = document.getElementById("fox");
         c.drawImage(img, this.x, this.y, 50, 50);
     }
 
@@ -40,7 +35,7 @@ class  Renard {
     }
 
     checkTTL() {
-        
+
     }
 
    SeDeplacent(){
@@ -60,16 +55,32 @@ class  Renard {
 }
 
 class Lapin {
-    constructor(actif, multiplicite, vitesse, freqApparition, nbLapin, x,y, id) {
-        this.actif = actif;
-        this.multiplicite = multiplicite;
-        this.vitesse = vitesse;
-        this.freqApparition = freqApparition;
-        this.nbLapin = nbLapin;
-        this.multiplicite = multiplicite
-        this.x = x;
-        this.y = y;
-        this.id = id;
+
+    constructor(coords, dx, dy, actif, multiplicite, vitesse, freqApparition, nbLapin, x,y, id) {
+        this.x = coords.x
+        this.y = coords.y
+        this.dx = dx;
+        this.dy = dy;
+        this.rayon = rayon;
+    }
+
+    creer() {
+        const img = document.getElementById("rabbit");
+        c.drawImage(img, this.x, this.y, 50, 50);
+    }
+
+    update() {
+        if (this.x + this.rayon > innerWidth || this.x - this.rayon < 0) {
+            this.dx = -this.dx;
+        }
+        if(this.y + this.rayon > innerHeight || this.y - this.rayon < 0)
+        {
+            this.dy = -this.dy; 
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.creer();
     }
 
     seMultiplier(){
@@ -92,69 +103,24 @@ canvas.height = window.innerHeight;
 
 // Context
 var c = canvas.getContext('2d');
-/*c.fillRect(100, 100, 100, 100);
-c.fillRect(200, 100, 100, 100);
-c.fillRect(300, 100, 100, 100);*/
-
-
-// Arc / Cercle
-/*var x, y;
-for (var i = 1; i < 100; i++) {
-    x = Math.random() * window.innerWidth;
-    y = Math.random() * window.innerHeight;
-
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
-}*/
-
-
-
-
 var rayon = 30;
 var y = 200;
-/*function animate() {
-    // Permet d'éxécuter la fonction animate plusieurs fois
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-
-    //renard.creer();
-
-    for (var i = 0; i < renards.length; i++) {
-        renards[i].update();
-    }
-
-    c.beginPath();
-    c.arc(x,y, rayon, 0, Math.PI * 2, false);
-    c.strokeStyle = 'blue';
-    c.stroke();
-
-    if (x + rayon > innerWidth || x - rayon < 0) {
-        dx = -dx;
-    }
-    if(y + rayon > innerHeight || y - rayon < 0)
-    {
-        dy = -dy; 
-    }
-    x += dx;
-    y += dy;
-}*/
-
-//animate();
 
 
 var app = {
     alredyStarted: false,
     // Nombre initiale de renard
     initialFoxNumber: 0,
+    initialRabbitNumber: 0,
     renards: [],
+    rabbits: [],
     start: () => {
         if (app.alredyStarted) app.reset();
         else app.alredyStarted = true;
 
         console.log('Application started !');
         app.initFox();
+        app.initRabbit();
         app.show();
         console.log(app.renards);
 
@@ -170,6 +136,19 @@ var app = {
             var dx = (Math.random() - 0.5) * 8;
             var dy = (Math.random() - 0.5) * 8;
             app.renards.push(new Renard({x: x, y: y}, dx, dy, rayon));
+        }
+    },
+    initRabbit: () => {
+        var irn = parseInt(document.getElementById('initialRabbitNumber').value);
+        app.initialRabbitNumber = (irn < 0 || isNaN(irn)) ? 10 : irn;
+
+        for (var i = 1; i <= app.initialRabbitNumber; i++) {
+            var rayon = 30;
+            var x = Math.random() * (innerWidth - rayon * 2) + rayon;
+            var y = Math.random() * (innerHeight - rayon * 2) + rayon;
+            var dx = (Math.random() - 0.5) * 8;
+            var dy = (Math.random() - 0.5) * 8;
+            app.rabbits.push(new Lapin({x: x, y: y}, dx, dy, rayon));
         }
     },
     updateFoxNumnber: () => {
@@ -188,6 +167,10 @@ var app = {
     
         for (var i = 0; i < app.renards.length; i++) {
             app.renards[i].update();
+        }
+
+        for (var i = 0; i < app.rabbits.length; i++) {
+            app.rabbits[i].update();
         }
     },
     reset: (what) => {
