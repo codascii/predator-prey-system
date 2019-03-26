744e07d81bc3fe5ac99c4f24b9806f00b28c5513
Codascii <dev.codascii@gmail.com>
2019-03-26 17:48:25 +0100

    Ajout de la fréquence d'apparition d'un Lapin

--- index.js
+++ index.js
@@ -112,6 +112,7 @@ var app = {
     // Nombre initiale de renard
     initialFoxNumber: 0,
     initialRabbitNumber: 0,
+    rabbitFrequency: 1,
     renards: [],
     rabbits: [],
     start: () => {
@@ -140,7 +141,7 @@ var app = {
     },
     initRabbit: () => {
         var irn = parseInt(document.getElementById('initialRabbitNumber').value);
-        app.initialRabbitNumber = (irn < 0 || isNaN(irn)) ? 10 : irn;
+        app.initialRabbitNumber = (irn < 0 || isNaN(irn)) ? 10 : irn;        
 
         for (var i = 1; i <= app.initialRabbitNumber; i++) {
             var rayon = 30;
@@ -157,6 +158,7 @@ var app = {
     },
     show: () => {
         app.animate();
+        app.generateRabbit();
     },
     animate: () => {
         // Permet d'éxécuter la fonction animate plusieurs fois
@@ -172,6 +174,19 @@ var app = {
         for (var i = 0; i < app.rabbits.length; i++) {
             app.rabbits[i].update();
         }
+    },
+    generateRabbit: () => {
+        var rf = parseInt(document.getElementById('rabbitFrequency').value);
+        app.rabbitFrequency = (rf < 0 || isNaN(rf)) ? 1 : rf;
+        
+        setInterval(() => {
+            var rayon = 30;
+            var x = Math.random() * (innerWidth - rayon * 2) + rayon;
+            var y = Math.random() * (innerHeight - rayon * 2) + rayon;
+            var dx = (Math.random() - 0.5) * 8;
+            var dy = (Math.random() - 0.5) * 8;
+            app.rabbits.push(new Lapin({x: x, y: y}, dx, dy, rayon));
+        }, app.rabbitFrequency * 1000);
     },
     reset: (what) => {
         console.log("reset");
